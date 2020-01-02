aae2c1b8a8417e8346afce0a35ad69f0144ef23b
Swaraj <swaraj.dehuri@gmail.com>
2020-01-02 23:15:44 +0530

    Product model added, basic view added

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    summary = models.TextField()
+    featured = models.BooleanField(default=True)
