5c3e27abfe4e0897c81051baec66b9c0bcdb7f14
Swaraj Kumar Dehuri <swaraj.dehuri@gmail.com>
2020-01-04 18:34:33 +0530

    for loop and if block in template

--- src/products/views.py
+++ src/products/views.py
@@ -1,17 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from products.models import Product
+
 
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World!</h1>")
+    product_object = Product.objects.get(id=1)
     context = {
-        'test': 'my context'
+        'title': product_object.title,
+        'price': product_object.price
     }
     return render(request, 'home.html', context)
 
 
 def about_view(request, *args, **kwargs):
     context = {
-        'test': 'my context'
+        'test': 'my context',
+        'list_item': [123, 345, 567, 789]
     }
     return render(request, 'about.html', context)
