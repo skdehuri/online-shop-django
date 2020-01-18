d714ad042b22f0c4fa3b38c5d81a1a83439ad5b0
Swaraj <swaraj.dehuri@gmail.com>
2020-01-18 18:37:46 +0530

    converted tab indent to space

--- src/snippets/models.py
+++ src/snippets/models.py
@@ -11,7 +11,7 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
-	creation_time = models.DateTimeField(auto_now_add=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
