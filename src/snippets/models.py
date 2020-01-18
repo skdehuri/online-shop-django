f9a85d691a0b561b2f89122d7411c32cd8618706
Swaraj Kumar Dehuri <swaraj.dehuri@gmail.com>
2020-01-18 18:25:47 +0530

    Revert "snippet model updated"

--- src/snippets/models.py
+++ src/snippets/models.py
@@ -11,8 +11,7 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
-	creation_time = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
