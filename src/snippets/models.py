ec48507bb396a3cdb2f7d90e7e31e75e233d5eca
Swaraj <swaraj.dehuri@gmail.com>
2020-01-18 17:34:00 +0530

    snippet model updated

--- src/snippets/models.py
+++ src/snippets/models.py
@@ -11,7 +11,7 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
 class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
@@ -21,7 +21,7 @@ class Snippet(models.Model):
     highlighted = models.TextField()
 
     class Meta:
-        ordering = ['created']
+        ordering = ['creation_time']
 
     def save(self, *args, **kwargs):
         """
